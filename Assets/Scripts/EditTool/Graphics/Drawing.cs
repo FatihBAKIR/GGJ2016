96b717c773f8dc0672f7b7f55bf97f34ec395226
Alper Keskin <alprkskn@gmail.com>
2016-01-30 20:40:34 +0200

    Tile indicator added. Tile delete ability added. Ortographic zoom added.

---
+++ Assets/Scripts/EditTool/Graphics/Drawing.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using f16sbe.Graphics.Lines;
+using f16sbe.Utils.CameraUtils;
+using f16sbe.Utils;
+
+public static class DrawingInterface
+{
+    #region DrawLine
+    public static void DrawLine(Vector3 start, Vector3 end)
+    {
+        DrawLine(start, end, Color.yellow, -1f);
+    }
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color color)
+    {
+        DrawLine(start, end, color, -1);
+    }
+
+    public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration)
+    {
+        var ln = Camera.main.GetComponent<LineHelper>().AddLine(start, end, color);
+        ln.lifeTime = duration;
+    }
+    #endregion
+
+    #region DrawPolyLine
+    public static void DrawPolyLine(List<Vector3> nodes)
+    {
+        DrawPolyLine(nodes, Color.yellow, -1);
+    }
+    public static void DrawPolyLine(List<Vector3> nodes, Color color)
+    {
+        DrawPolyLine(nodes, color, -1);
+    }
+    public static void DrawPolyLine(List<Vector3> nodes, Color color, float duration, bool isClosed = false, bool isAlternating = false)
+    {
+        var pLine = new PolyLine(nodes, color, isClosed, isAlternating);
+        pLine.lifeTime = duration;
+        Camera.main.GetComponent<LineHelper>().AddPolyLine(pLine);
+    }
+    #endregion
+}
---
+++ Assets/Shaders/LineShader.shader
@@ -0,0 +1,7 @@
+﻿Shader "Lines/Colored Blended" {
+SubShader{ Pass {
+	Blend SrcAlpha OneMinusSrcAlpha
+	ZWrite On Cull Off Fog { Mode Off }
+	BindChannels {
+	  Bind "vertex", vertex Bind "color", color }
+} } }
