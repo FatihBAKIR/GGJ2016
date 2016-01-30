203bde7f85e1c935742c15e3f1abf70b6e946168
Fatih BAKIR <fatih@linux.com>
2016-01-30 11:19:26 +0200

    Small fix, grass tile

--- Assets/CubeGen.cs
+++ Assets/CubeGen.cs
@@ -18,7 +18,9 @@ public static class CubeGen
     // Use this for initialization
     static Mesh GenerateCube()
     {
-        Mesh m = GameObject.CreatePrimitive(PrimitiveType.Cube).GetComponent<MeshFilter>().mesh;
+        var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Mesh m = go.GetComponent<MeshFilter>().sharedMesh;
+        Object.Destroy(go);
 
         Vector2[] uvs = new Vector2[24];
 
@@ -32,11 +34,6 @@ public static class CubeGen
         uvs[2] = uvs[11] = uvs[13] = uvs[17] = uvs[21] = new Vector2(0.5f, 1);
         uvs[3] = uvs[10] = uvs[14] = uvs[18] = uvs[22] = new Vector2(1, 1);
 
-        //Debug.DrawLine(m.vertices[20], m.vertices[20] + m.normals[20], Color.blue, 600);
-        //Debug.DrawLine(m.vertices[21], m.vertices[21] + m.normals[21], Color.green, 600);
-        //Debug.DrawLine(m.vertices[22], m.vertices[22] + m.normals[22], Color.red, 600);
-        //Debug.DrawLine(m.vertices[23], m.vertices[23] + m.normals[23], Color.yellow, 600);
-
         uvs[9] = new Vector2(0, 0);
         uvs[8] = new Vector2(0.5f, 0);
         uvs[5] = new Vector2(0, 1f);
@@ -45,10 +42,6 @@ public static class CubeGen
         m.uv = uvs;
         m.UploadMeshData(false);
 
-        //for (int i = 0; i < m.vertexCount; i++)
-        //{
-        //    Debug.Log(i + " - " + m.normals[i]);
-        //}
         return m;
     }
 }
