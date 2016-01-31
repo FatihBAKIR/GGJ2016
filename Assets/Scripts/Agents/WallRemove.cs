12919db767956ee3814ad2abed4a9435b57b8084
Fatih BAKIR <fatih@linux.com>
2016-01-31 09:51:38 +0200

    Spell images

--- Assets/Scripts/Agents/WallRemove.cs
+++ Assets/Scripts/Agents/WallRemove.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Linq;
 
-public class WallRemove : Agent {
-    
+public class WallRemove : Agent
+{
+    private bool _done;
     protected override void DoStep()
     {
+        if (_done)
+        {
+            return;
+        }
+
         if (Level.CurrentLevel.Get(Position).AgentsOnTile(agent => agent is Player).Length > 0)
         {
+            _done = true;
             Debug.Log(string.Format("{0} is destroyed!", Target));
             Level.CurrentLevel.Destroy(FindObjectsOfType<Agent>().FirstOrDefault(agent => agent.Id == Target));
             Destroy(transform.GetChild(0).gameObject);
